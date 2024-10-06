ba7252124153df063a8ddcd79b5474281f2fbaa4
Таисия Харинова <taisiya.kharinova@mail.ru>
2024-10-06 17:07:10 +0500

    Задание 2 выполнено(21 варинат)

---
+++ task02/task02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны правильного икосаэдра");
+            double a = double.Parse(Console.ReadLine());
+
+            double x = Math.Pow(a, 2);
+            double c = 3;
+            double y = Math.Sqrt(c);
+            double s = 5 * x * y;
+            double k = Math.Pow(a, 3);
+            double b = 5;
+            double z = Math.Sqrt(b);
+            double v = (5 * k * (3 + z)) / 12;
+
+            Console.WriteLine("Площадь икосаэдра: " + Math.Round(s, 2));
+            Console.WriteLine("Объём икосаэдра: " + Math.Round(v, 2));
+
+            Console.ReadKey();
+        }
+    }
+}

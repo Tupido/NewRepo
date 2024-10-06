f681f7c4ff412eea6f63068acba7f8985041b374
Таисия Харинова <taisiya.kharinova@mail.ru>
2024-10-06 17:53:34 +0500

    Задание 3 выполнено (21 вариант)

---
+++ task03/task03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите четырехзначное число");
+            var number = int.Parse(Console.ReadLine());
+
+            var last = number % 100;
+            var third = last / 10;
+            var cut = number / 10;
+            var result = third * 1000 + cut;
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}

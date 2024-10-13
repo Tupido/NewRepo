8b6a9d1e85eb4ec63c4fd7c9f7aa6ba7727040e8
Таисия Харинова <taisiya.kharinova@mail.ru>
2024-10-13 18:32:11 +0500

    Задание 5 выполнено (21 вариант)

---
+++ task05/task05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = F(1, 2) + F(2, 2) + F(3, 5) + F(4, 3);
+            Console.WriteLine("x = " + Math.Round(y, 3));
+
+            Console.ReadKey();
+        }
+        static double F(double a, double b)
+        {
+            return (a+Math.Exp(-a))/b;
+        }
+    }
+}

36c6b823eec1f8d8029dcc2f6a1827df2aace9ae
Taisia.Kharinova <charinova1306@gmail.com>
2024-10-14 13:50:36 +0500

    Задание 6 выполнено

---
+++ task06/task6.1/task6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "перевозка";
+            var word1 = s
+                .Remove(5)
+                .Remove(0, 1)
+                .Remove(0, 0);
+
+            word1 += s.Substring(6); 
+
+            Console.WriteLine(word1);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

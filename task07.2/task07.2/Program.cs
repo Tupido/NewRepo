ae0414867f2729fa324349c45f1c9b74bbc06266
Taisia.Kharinova <charinova1306@gmail.com>
2024-10-21 13:40:15 +0500

    Задание 7.2

---
+++ task07.2/task07.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки через пробел");
+            var input = Console.ReadLine();
+
+            var k = input.IndexOf(" ");
+            var x = double.Parse(input.Substring(0, k));
+            var y = double.Parse(input.Substring(k + 1));
+
+            Console.WriteLine($"Введена точка({x}; {y})");
+
+            if (PointInArea(x, y))
+                Console.WriteLine("Лежит");
+            else 
+                Console.WriteLine("Не лежит");
+        }
+
+        static bool PointInArea(double x,  double y) 
+        {
+            return ();
+        }
+    }
+}

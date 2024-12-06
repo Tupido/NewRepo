79da489e4912af564b753d5961e8c1381e225aaa
Таисия Харинова <taisiya.kharinova@mail.ru>
2024-12-06 19:07:35 +0500

    задание 10-2 выполнено (21 вариант)

---
+++ task10-2/task10-2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество студентов");
+            int m;
+
+            if (!TryInputNumber(out m) || m <= 0)
+            {
+                Console.ReadKey();
+                return;
+            }
+            
+            Console.WriteLine("Введите количество экзаменов");
+            int k;
+
+            if (!TryInputNumber(out k) || k <= 0)
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            for (int i = 1; i <= m; i++)
+            {
+                int totalScore = 0;
+                Console.WriteLine($"Введите оценки студента {i} через пробел:");
+
+                for (int j = 1; j <= k; j++)
+                {
+                    int score;
+                    if (!TryInputNumber(out score) || score < 0 || score > 100)
+                    {
+                        Console.WriteLine("Оценка должна соответствовать 100 бальной шкале");
+                        Console.ReadKey();
+                        return;
+                    }
+                    totalScore += score;
+                }
+                Console.WriteLine($"Сумма баллов студента {i} составляет {totalScore}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool TryInputNumber(out int number)
+        {
+            number = 0;
+            if (!int.TryParse(Console.ReadLine(), out int n))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+
+            number = n;
+            return true;
+        }
+    }
+}

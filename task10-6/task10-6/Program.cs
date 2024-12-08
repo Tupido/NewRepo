9e227d2ee7a7211668019bf2fe8d7a4780f78926
Таисия Харинова <taisiya.kharinova@mail.ru>
2024-12-08 15:47:59 +0500

    задание 10-6 выполнено (21 вариант)

---
+++ task10-6/task10-6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalMoney = 100; 
+            int totalHeads = 100; 
+
+            int bullPrice = 10; 
+            int cowPrice = 5;   
+            double calfPrice = 0.5; 
+
+            for (int bulls = 0; bulls <= totalMoney / bullPrice; bulls++)
+            {
+                for (int cows = 0; cows <= (totalMoney - bulls * bullPrice) / cowPrice; cows++)
+                {
+                    int remainingMoney = totalMoney - (bulls * bullPrice + cows * cowPrice);
+                    int calves = (int)(remainingMoney / calfPrice);
+
+                    if (bulls + cows + calves == totalHeads)
+                    {
+                        Console.WriteLine($"Быков: {bulls}, Коров: {cows}, Теленков: {calves}");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

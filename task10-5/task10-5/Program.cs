d1086c27c25b69b530cb729d33d7ad6ab034789b
Таисия Харинова <taisiya.kharinova@mail.ru>
2024-12-08 14:28:16 +0500

    задание 10-5 выполнено (21 вариант)

---
+++ task10-5/task10-5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число");
+
+            int number;
+
+            if (!int.TryParse(Console.ReadLine(), out number) || number < 1)
+            {
+                Console.WriteLine("Ошибка ввода");
+                Console.ReadKey();
+                return;
+            }
+
+            int smallestDivisor = -1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    smallestDivisor = i;
+                    break;
+                }
+            }
+
+            if (smallestDivisor != -1)
+            {
+                Console.WriteLine($"Наименьший делитель {number}, отличный от 1: {smallestDivisor}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
